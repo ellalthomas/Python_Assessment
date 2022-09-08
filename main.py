9514792f9af57f46ff29b19ee563c840104fbf41
ella thomas <78177131+ellalthomas@users.noreply.github.com>
2022-09-08 22:19:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Press A to add a book:")
+print("Press M to modify the number of pages of a book:")
+print("Press L to print the number of pages of a book:")
+print("Press P to Print out all books’ titles with their authors and the number of pages:")
+print("Press Q to quit:")
+
+books = {
+  "book1": {"author": "Ella",
+            "pages": "200"},
+    "book2": {"author": "Lily",
+            "pages": "5"},
+    "book3": {"author": "Dayna",
+            "pages": "420"},
+    "book4": {"author": "Kistina",
+            "pages": "69"},
+}
+print(books)
