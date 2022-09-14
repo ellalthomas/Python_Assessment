dc5daed5e73f97a5dc512fd0fbd87b93edc8bbfb
ella thomas <78177131+ellalthomas@users.noreply.github.com>
2022-09-14 23:25:09 +0000

    Added book list to put user inputs in

--- main.py
+++ main.py
@@ -16,18 +16,26 @@
 # }
 # print(books)
 
+books = []
+
 quit = True
 
 while quit == True:
-  menu = input("Press A to add a book:\n Press M to modify the number of pages of a book: \n Press L to print the number of pages of a book: \n Press P to Print out all books’ titles with their authors and the number of pages: \n Press Q to quit:")
+  menu = input("Press A to add a book:\n Press M to modify the number of pages of a book: \n Press L to print the number of pages of a book: \n Press P to Print out all books’ titles with their authors and the number of pages: \n Press Q to quit:\n")
   if menu == "A":
-    print("hi")
+    new_book = input("What is the name of the book?")
+    author = input("Who is the author of the book?")
+    pages = input("How many pages does the book have?")
+    books.append(new_book)
+    books.append(author)
+    books.append(pages)
+    
   elif menu == "M":
     print("yo")
   elif menu == "L":
     print("Kia ora")
   elif menu == "P":
-    print("sup")
+    print(books)
   elif menu == "Q":
     quit = False
 
